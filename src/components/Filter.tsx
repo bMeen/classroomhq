1835899ebcea68e905b0ef9a847c4e6112e95ade
Al-Ameen <belloalameenn@outlook.com>
2025-05-24 19:34:05 +0100

    type fix

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -16,7 +16,7 @@ function Filter({ field, options }: { field: string; options: Options[] }) {
         <button
           className={`rounded-md px-3 py-1 transition-all duration-75 ${currentField === value ? "cursor-not-allowed bg-black text-white" : "cursor-pointer bg-none text-black"}`}
           key={value}
-          onClick={() => handleFilter(value)}
+          onClick={() => handleFilter(value as string)}
           disabled={currentField === value}
         >
           {label}
